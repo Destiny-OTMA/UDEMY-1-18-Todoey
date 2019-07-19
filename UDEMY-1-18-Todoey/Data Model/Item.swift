b36393cc2bcf46b0f62f6434d586e60ad3f31338
Destiny Sopha <destiny@sopha.org>
2019-07-19 13:59:46 -0400

    Convert to MVC design pattern

---
+++ UDEMY-1-18-Todoey/Data Model/Item.swift
@@ -0,0 +1,23 @@
+//
+//  Item.swift
+//  UDEMY-1-18-Todoey
+//
+//  Created by Destiny Sopha on 7/19/2019.
+//  Copyright © 2019 Destiny Sopha. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+  var title: String = ""
+  var done: Bool = false
+}
+
+
+
+
+
+
+
+
+
